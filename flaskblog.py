a973b414b0caf89d4e1c62967abde8df82d5dff1
Jason Bruno <jasondreams0513@gmail.com>
2019-07-27 15:59:28 -0700

    Adds login.html/register.html. Adds routes for login/register.

--- flaskblog.py
+++ flaskblog.py
@@ -281,6 +281,14 @@ def gallery():
 def about():
     return render_template('about.html', title='About')
 
+@app.route("/register")
+def register():
+    return render_template('register.html', title='Register')
+
+@app.route("/login")
+def login():
+    return render_template('login.html', title='Login')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
