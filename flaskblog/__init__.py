acd5bd43729397588592927861baa4e9394d75db
Jason Bruno <jasondreams0513@gmail.com>
2019-08-07 15:36:55 -0700

    Sets up configuration by setting environmental variables properly. Also moves variablesto a separate config file.

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -6,21 +5,10 @@ from flask_login import LoginManager
 from flask_admin import Admin
 from flask_basicauth import BasicAuth
 from flask_mail import Mail
-
+from flaskblog.config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5731628ba0f13ce0c643fde283baw451'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['BASIC_AUTH_USERNAME'] = 'username'
-app.config['BASIC_AUTH_PASSWORD'] = 'password'
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = "username"
-app.config['MAIL_PASSWORD'] = "password"
-# app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-# app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
-
+app.config.from_object(Config)
 mail = Mail(app)
 
 db = SQLAlchemy(app)
