4a19a0fe471c106170f954fca5af35e2c48a593c
Jason Bruno <jasondreams0513@gmail.com>
2019-08-09 16:22:35 -0700

    Adds database creation to init

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -7,6 +7,7 @@ from flask_basicauth import BasicAuth
 from flask_mail import Mail
 from flaskblog.config import Config
 
+
 mail = Mail()
 
 db = SQLAlchemy()
@@ -20,6 +21,7 @@ login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'dark'
 
+
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
@@ -31,14 +33,22 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     login_manager.init_app(app)
 
+
     from flaskblog.users.routes import users
     from flaskblog.posts.routes import posts
     from flaskblog.main.routes import main
     from flaskblog.errors.handlers import errors
+    from flaskblog.gallery.routes import gall
 
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
     app.register_blueprint(errors)
+    app.register_blueprint(gall)
+
+
+    with app.app_context():
+        db.create_all()
+
 
     return app
