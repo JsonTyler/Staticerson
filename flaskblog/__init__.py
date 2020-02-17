85ed64216682ee2630e4df2abcf4bc4c4beaf476
Jason Bruno <jasondreams0513@gmail.com>
2020-02-17 10:29:27 -0800

    Current version of website w/o blog or working contact form

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -18,7 +18,6 @@ basic_auth = BasicAuth()
 
 bcrypt = Bcrypt()
 login_manager = LoginManager()
-login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'dark'
 
 
@@ -33,19 +32,10 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     login_manager.init_app(app)
 
-
-    from flaskblog.users.routes import users
-    from flaskblog.posts.routes import posts
-    from flaskblog.main.routes import main
     from flaskblog.errors.handlers import errors
-    from flaskblog.gallery.routes import gall
     from flaskblog.project.routes import proj
-
-    app.register_blueprint(users)
-    app.register_blueprint(posts)
-    app.register_blueprint(main)
+    
     app.register_blueprint(errors)
-    app.register_blueprint(gall)
     app.register_blueprint(proj)
 
     with app.app_context():
--- flaskblog/project/routes.py
+++ flaskblog/project/routes.py
@@ -5,16 +5,21 @@ from flaskblog import db, basic_auth
 from flask_basicauth import BasicAuth
 from flaskblog.models import Project
 from flaskblog.project.forms import ProjectForm
-from flaskblog.users.utils import save_picture
 
 proj = Blueprint('proj', __name__)
+
+# email stuff
+port = 465
+username = "jasondreams0513@gmail.com"
+receiver_email = "jasondreams0513@gmail.com"
 
 #new
 project = ""
 
-@proj.route("/project")
+@proj.route("/")
 def project():
     project = Project.query.order_by(Project.title.desc())
-    return render_template('projects.html', title='Projects', project=project)
+    projects = Project.query.all()
+    return render_template('test.html', title='Projects', project=project)
 
 # data
